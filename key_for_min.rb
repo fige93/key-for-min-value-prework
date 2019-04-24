951c2cfdb6b7485f323a819ba5691d5284362e3e
Felix Benetzeder <felix.benetzeder@blackrock.com>
2019-04-24 15:23:47 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -1,24 +1,11 @@
-def key_for_min_value(name_hash)
-  if name_hash == {}
-    return nil
-  elsif
-<<<<<<< HEAD
-    minimum = 15
-    minimumKey = ""
-    name_hash.each do |key, value|
-      if value < minimum
-        minimumKey = key
-        value = minimum
-=======
-    min
-    minimumKey = "name_hash.keys[0]"
-    name_hash.each do |key, value|
-      if value < minimum
-        value = minimum
-        minimumKey = key
->>>>>>> 29b5129555ad4215df6adae4a5aa52362b558c46
-      end
+def key_for_min_value(hash)
+  lowest_key = nil
+  lowest_value = nil
+  hash.each do |k, v|
+    if lowest_value == nil || v < lowest_value
+      lowest_value = v
+      lowest_key = k
     end
-    return minimumKey
   end
+  lowest_key
 end
